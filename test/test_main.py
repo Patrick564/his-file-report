0ec6756d06d24ed5c2aabc6163d3ab0fdbc54d3e
Patrick <pvilchez794@gmail.com>
2023-07-15 14:11:47 -0500

    Pytest add

---
+++ test/test_main.py
@@ -0,0 +1,19 @@
+from typer.testing import CliRunner
+
+from reportgen.cli import app
+
+runner = CliRunner()
+
+
+def test_search() -> None:
+    result = runner.invoke(app, ["search", "54435297", "-d"])
+    result_arg = runner.invoke(app, ["search", "54435297"])
+
+    assert result.exit_code == 0
+    assert result_arg.exit_code == 0
+
+
+def test_version() -> None:
+    result = runner.invoke(app, ["version"])
+
+    assert result.exit_code == 0
